cee5149dcda5c8750a462eb550a3e668365ffac5
CansuTeymur <cansuteymur1212@gmail.com>
2023-04-17 17:02:00 +0300

    Takvim Oluşturma

---
+++ 3 -- Takvim Oluşturma/takvim.py
@@ -0,0 +1,25 @@
+
+#Kullanıcının girdiği yıl ve ay bilgisine göre o ay ve yıla ait takvim oluşturma.
+
+
+import calendar
+
+yy=int(input("Enter year : "))
+mm=int(input("Enter month : "))
+
+print("\n",calendar.month(yy,mm))
+
+
+
+"""
+Enter year : 2023
+Enter month : 4
+
+      April 2023
+Mo Tu We Th Fr Sa Su
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+"""
